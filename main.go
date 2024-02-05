15c251e8254d655ed142346fb0c6635f748f7f83
timotewb <timote.wb@gmail.com>
2024-02-06 08:59:14 +1300

    build and test

--- main.go
+++ main.go
@@ -26,6 +26,20 @@ func main() {
 		)
 		log.Fatal(err)
 	}
+	outDir, err := zenity.SelectFile(
+		zenity.Filename(""),
+		zenity.Directory(),
+		zenity.DisallowEmpty(),
+		zenity.Title("Select output directory"),
+	)
+	if err != nil {
+		zenity.Error(
+			err.Error(),
+			zenity.Title("Error"),
+			zenity.ErrorIcon,
+		)
+		log.Fatal(err)
+	}
 
 	files, err := os.ReadDir(inDir)
 	if err != nil {
@@ -35,7 +49,10 @@ func main() {
 	for _, file := range files {
 		if file.Type().IsRegular() {
 			fileNameWithoutExt := strings.TrimSuffix(filepath.Base(file.Name()), filepath.Ext(file.Name()))
-			cmdStr := fmt.Sprintf("flatpak run --command=HandBrakeCLI fr.handbrake.ghb -v 1 -i \"%s\" -o \"/mnt/ns00/Complete/%s\" -Z \"Apple 2160p60 4K HEVC Surround\" --encoder nvenc_h265", filepath.Join(inDir, file.Name()), fileNameWithoutExt)
+			inFile := filepath.Join(inDir, file.Name())
+			outFile := filepath.Join(outDir, fileNameWithoutExt+".m4v")
+			cmdStr := fmt.Sprintf("flatpak run --command=HandBrakeCLI fr.handbrake.ghb -v 1 -i \"%s\" -o \"%s\" -Z \"Apple 2160p60 4K HEVC Surround\" --encoder nvenc_h265", inFile, outFile)
+			fmt.Println(cmdStr)
 
 			// Create a new command
 			cmd := exec.Command("/bin/sh", "-c", cmdStr)
@@ -51,7 +68,7 @@ func main() {
 		}
 	}
 
-	zenity.Info("Folders renamed!",
+	zenity.Info("Files converted!",
 		zenity.Title("Complete"),
 		zenity.InfoIcon,
 	)
