e07b455f59e5586c9a343df87161f7c41bde1e9e
timotewb <timote.wb@gmail.com>
2024-03-12 08:44:32 +1300

    update to read title data

--- app/app.go
+++ app/app.go
@@ -7,9 +7,17 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"regexp"
+	"strconv"
 	"strings"
+	"unicode"
 )
+
+type Title struct {
+	Index         int `json:"index"`
+	Duration      int `json:"duration"`
+	AudioCount    int `json:"audio_count"`
+	SubtitleCount int `json:"subtitle_count"`
+}
 
 // Encode processes files in the specified input directory, encoding them according to the specified output directory.
 // It supports encoding of ISO files to extract and encode individual titles, as well as encoding of other video files.
@@ -65,21 +73,40 @@ func Encode(inDir, outDir string, ignore []string, drun bool) {
 					fmt.Printf("Failed to execute command: %v", err)
 				}
 
-				output := buf.String()
-				// Define the regex pattern
-				pattern := regexp.MustCompile(`\+ title [0-9]+:`)
-
-				// Find all matches of the pattern
-				matches := pattern.FindAllString(output, -1)
+				// Search through scan output for titles
+				titles, err := getTitles(buf.String(), 600)
+				if err != nil {
+					fmt.Println(err)
+				}
 
 				// Count the number of matches
-				count := len(matches)
+				count := len(titles)
 				fmt.Printf("- Found %d valid titles.\n", count)
 
-				for i := 1; i <= count; i++ {
+				for i := 0; i < count; i++ {
+
+					// audio tracks
+					var at string
+					for a := 1; a <= titles[i].AudioCount; a++ {
+						if at == "" {
+							at = fmt.Sprintf("%v", a)
+						} else {
+							at = fmt.Sprintf("%v,%v", at, a)
+						}
+					}
+
+					// subtitles
+					var sb string
+					for s := 1; s <= titles[i].AudioCount; s++ {
+						if sb == "" {
+							sb = fmt.Sprintf("%v", s)
+						} else {
+							sb = fmt.Sprintf("%v,%v", sb, s)
+						}
+					}
 
 					outFile := filepath.Join(outDir, fmt.Sprintf("%s%02d.m4v", fileNameWithoutExt, i))
-					cmdStr = fmt.Sprintf("flatpak run --command=HandBrakeCLI fr.handbrake.ghb -v 1 -i \"%s\" -o \"%s\" -Z \"Apple 2160p60 4K HEVC Surround\" --encoder nvenc_h265 --title %d", inFile, outFile, i)
+					cmdStr = fmt.Sprintf("flatpak run --command=HandBrakeCLI fr.handbrake.ghb -v 1 -i \"%s\" -o \"%s\" -Z \"Apple 2160p60 4K HEVC Surround\" --encoder nvenc_h265 --title %d -a \"%v\" -s \"%v\"", inFile, outFile, titles[i].Index, at, sb)
 
 					fmt.Print("  - Execute command:\n")
 					fmt.Printf("\t%s\n\n", cmdStr)
@@ -175,4 +202,136 @@ func contains(list []string, target string) bool {
 		}
 	}
 	return false
+}
+
+func getTitles(text string, minDurSec int) ([]Title, error) {
+
+	var titles []Title
+
+	// Split the text into lines
+	lines := strings.Split(text, "\n")
+
+	// Loop through each line
+	record := false
+	subs := false
+	subCount := 0
+	audios := false
+	audioCount := 0
+	var index int
+	var duration int
+	var err error
+	for _, line := range lines {
+		// Test if we have reached the title details
+		if strings.HasPrefix(line, "+ title ") {
+			// output
+			// 	fmt.Printf(" - Index: %v\n", index)
+			// 	fmt.Printf(" - Duration: %v\n", duration)
+			// 	fmt.Printf(" - Audio Count: %v\n", audioCount)
+			// 	fmt.Printf(" - Subtitle Count: %v\n\n", subCount)
+
+			if index > 0 && duration >= minDurSec && audioCount > 0 {
+				t := Title{
+					Index:         index,
+					Duration:      duration,
+					AudioCount:    audioCount,
+					SubtitleCount: subCount,
+				}
+				titles = append(titles, t)
+			}
+			record = true
+			subs = false
+			subCount = 0
+			audios = false
+			audioCount = 0
+			duration = 0
+		} else if strings.Contains(line, "HandBrake has exited.") {
+			// output
+			// 	fmt.Printf(" - Index: %v\n", index)
+			// 	fmt.Printf(" - Duration: %v\n", duration)
+			// 	fmt.Printf(" - Audio Count: %v\n", audioCount)
+			// 	fmt.Printf(" - Subtitle Count: %v\n\n", subCount)
+			if index > 0 && duration >= minDurSec && audioCount > 0 {
+				t := Title{
+					Index:         index,
+					Duration:      duration,
+					AudioCount:    audioCount,
+					SubtitleCount: subCount,
+				}
+				titles = append(titles, t)
+			}
+			record = false
+		}
+
+		if record {
+			// Get index
+			if strings.Contains(line, "+ index") {
+				// Convert the string to an integer
+				index, err = strconv.Atoi(removeNonNumeric(line))
+				if err != nil {
+					return titles, err
+				}
+				subs = false
+				audios = false
+			}
+			// Test for subtitles
+			if strings.Contains(line, "+ subtitle tracks:") {
+				subs = true
+				audios = false
+			} else if subs {
+				subCount += 1
+			}
+			// Test for audio tracks
+			if strings.Contains(line, " + audio tracks:") {
+				audios = true
+				subs = false
+			} else if audios {
+				audioCount += 1
+			}
+			// Test for duration
+			if strings.Contains(line, "+ duration:") {
+				duration, err = durationToSeconds(strings.TrimSpace(strings.Replace(line, "+ duration:", "", 1)))
+				if err != nil {
+					return titles, err
+				}
+				subs = false
+				audios = false
+			}
+		}
+	}
+	return titles, nil
+}
+
+func removeNonNumeric(s string) string {
+	var builder strings.Builder
+	for _, r := range s {
+		if unicode.IsDigit(r) {
+			builder.WriteRune(r)
+		}
+	}
+	return builder.String()
+}
+
+func durationToSeconds(duration string) (int, error) {
+	parts := strings.Split(duration, ":")
+	if len(parts) != 3 {
+		return 0, fmt.Errorf("invalid duration format")
+	}
+
+	hours, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return 0, err
+	}
+
+	minutes, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return 0, err
+	}
+
+	seconds, err := strconv.Atoi(parts[2])
+	if err != nil {
+		return 0, err
+	}
+
+	totalSeconds := hours*3600 + minutes*60 + seconds
+	return totalSeconds, nil
 }
